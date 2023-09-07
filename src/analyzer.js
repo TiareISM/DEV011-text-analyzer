44b65751f0ada845feee46964891e6f8df063623
TiareISM <140457571+TiareISM@users.noreply.github.com>
2023-09-07 10:17:27 -0300

    Feature/text analyzer (#2)
    * añadir funcion para contar la media de las palabras
    
    * agregando ultimas funciones
    
    * agregando readme

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,6 +1,6 @@
 const analyzer = {  
   getWordCount: (text) => {
-    const words = text.split(' ');
+    const words = text.trim().split(/\s+/);
     return words.length
   },
   
@@ -11,13 +11,44 @@ const analyzer = {
     return text.replace( /[,.\s]/g, '' ).length;
   },
   getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split (' ');
+    const totalLength = words.reduce((acc, word) => acc + word.length, 0);
+    const averageLength = totalLength / words.length;
+    return Number(averageLength.toFixed(2));
+    
+  
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numberCount = text.match(/\b\d+(\.\d+)?\b/g);
+    if (numberCount) {
+      return numberCount.length;
+    }
+    else {
+      return 0;
+    }
+   
+    
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numberCount = text.match(/\b\d+(\.\d+)?\b/g);
+    if (numberCount){
+      let sumaNumber = 0;
+
+      for (let i = 0; i < numberCount.length; i++){
+        const numero = parseFloat(numberCount[i]);
+        if (!isNaN(numero)){
+          sumaNumber += numero;
+        }
+    
+      }
+      return  isNaN (sumaNumber) ? 0: sumaNumber;
+    } else {
+      return 0;
+    }
+    
+
+
+  
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,52 @@
 import analyzer from './analyzer.js';
 
 const input = document.querySelector('[name="user-input"]');
-const wordCountList = document.querySelector('[data-testid="word-count"]');
+const wordCountList = document.querySelector('li[data-testid="word-count"]');
 const resetButton = document.getElementById ('reset-button');
-const characterCountList = document.querySelector('[data-testid="character-count"]')
-const characterWithoutSpaceList = document.querySelector('[data-testid="character-no-spaces-count"]')
+const characterCountList = document.querySelector('li[data-testid="character-count"]');
+const characterWithoutSpaceList = document.querySelector('li[data-testid="character-no-spaces-count"]');
+const wordlengthaverageList = document.querySelector('li[data-testid="word-length-average"]');
+const numberCountList = document.querySelector('li[data-testid="number-count"]');
+const sumNumberList = document.querySelector('li[data-testid="number-sum"]');
+
+
 
 input.addEventListener('keyup', () => {
   const userInput = input.value;
   if(userInput === ''){
-    wordCountList.textContent = 'Conteo de Palabras: 0';
+    wordCountList.textContent = 'Palabras: 0';
     characterCountList.textContent = 'Caracteres: 0';
     characterWithoutSpaceList.textContent = 'Caracteres sin Espacios: 0';
+    wordlengthaverageList.textContent = 'Longitud promedio palabras: 0';
+    numberCountList.textContent = 'Números: 0';
+    sumNumberList.textContent = 'Suma números: 0';
 
   } else {
     const wordCount = analyzer.getWordCount(userInput);
-    wordCountList.textContent = `Conteo de Palabras: ${wordCount}`;
+    wordCountList.textContent = `Palabras: ${wordCount}`;
     const characterCount = analyzer.getCharacterCount(userInput);
     characterCountList.textContent = `Caracteres: ${characterCount}`;
     const characterWithoutSpaceCount = analyzer.getCharacterCountExcludingSpaces(userInput);
     characterWithoutSpaceList.textContent = `Caracteres sin Espacios: ${characterWithoutSpaceCount}`;
+    const averageLength = analyzer.getAverageWordLength (userInput);
+    wordlengthaverageList.textContent = `Longitud promedio palabras: ${averageLength}`;
+    const numberCount = analyzer.getNumberCount(userInput);
+    numberCountList.textContent = `Números: ${numberCount}`;
+    const sumaNumber = analyzer.getNumberSum(userInput);
+    sumNumberList.textContent = `Suma números: ${sumaNumber}`;
+
+
   }
   
 });
 
 resetButton.addEventListener ('click', () => {
   input.value = '';
-  wordCountList.textContent = 'Conteo de Palabras: 0';
+  wordCountList.textContent = 'Palabras: 0';
   characterCountList.textContent = 'Caracteres: 0';
   characterWithoutSpaceList.textContent = 'Caracteres sin Espacios: 0';
+  wordlengthaverageList.textContent = 'Longitud promedio palabras: 0';
+  numberCountList.textContent = 'Números: 0';
+  sumNumberList.textContent = 'Suma números: 0';
 
 });
